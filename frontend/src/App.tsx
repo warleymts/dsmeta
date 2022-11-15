f6ad99f4a4fe51dbc79dd8e0628ca56590fe5a4f
Warley Martins <warlinpb@outlook.com>
2022-11-15 00:20:25 -0300

    First component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,13 @@
-
+import NotificationButton from "./components/NotificationButton";
 
 function App() {
-  return(
-    <h1>Hello, World!</h1>
+  return (
+    <>
+      <h1>Hello, World!</h1>
+      <NotificationButton />
+      <NotificationButton />
+    </>
   )
 }
 
-export default App
+export default App;
