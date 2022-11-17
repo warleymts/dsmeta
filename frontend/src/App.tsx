dcdb34f9b9c539b23508e7a6f08d28bb1ec1fbaa
Warley Martins <warlinpb@outlook.com>
2022-11-16 22:49:06 -0300

    Other components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,20 @@
-import NotificationButton from "./components/NotificationButton";
+import Header from "./components/Header";
+import SalesCard from "./components/SalesCard";
+
 
 function App() {
   return (
     <>
-      <h1>Hello, World!</h1>
-      <NotificationButton />
-      <NotificationButton />
+      <Header />
+      <main>
+        <section id="sales">
+          <div className="dsmeta-container">
+            <SalesCard />
+          </div>
+        </section>
+      </main>
     </>
-  )
+  );
 }
 
 export default App;
