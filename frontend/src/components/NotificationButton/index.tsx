eb13f3474f987747927d72e655d95bd1b13e6083
Warley Martins <warlinpb@outlook.com>
2022-11-19 00:15:19 -0300

    Toast

--- frontend/src/components/NotificationButton/index.tsx
+++ frontend/src/components/NotificationButton/index.tsx
@@ -1,4 +1,5 @@
 import axios from "axios";
+import { toast } from "react-toastify";
 import icon from "../../assets/img/notification-icon.svg";
 import { BASE_URL } from "../../utils/request";
 import "./styles.css";
@@ -7,9 +8,9 @@ type Props = {
   saleId: number;
 };
 
-function handleClick(id : number) {
-  axios(`${BASE_URL}/sales/${id}/notification`).then(response => {
-    alert ("Mensagem enviada com sucesso")
+function handleClick(id: number) {
+  axios(`${BASE_URL}/sales/${id}/notification`).then((response) => {
+    toast.info("SMS enviado com sucesso");
   });
 }
 
